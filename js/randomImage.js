178e3f6ecbc07e8f0833e80621f5f81929f201e5
Jaanek Rits <jrits@tktk.ee>
2022-11-30 19:55:56 +0200

    JavaScriptSandbox

---
+++ js/randomImage.js
@@ -0,0 +1,16 @@
+const myButton = document.querySelector('button');
+const myBox = document.querySelector('.box');
+const image = document.querySelector('img');
+const images = ["pic1.jpeg", "pic2.jpeg", "pic3.jpeg", "pic4.webp"];
+
+myButton.addEventListener('click', changeImage);
+
+function changeImage() {
+
+  
+    let randomIndex = Math.floor(Math.random() * images.length);
+    console.log(randomIndex);
+
+ 
+    image.src = 'images/'+images[randomIndex];
+}
